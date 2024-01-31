f3698eac76ee4399760503b59613cb8e1fc8435e
Murilo <mbalves1@outlook.com>
2024-01-31 13:55:01 -0300

    [feat]: criando rota para gerar a mensagem com openai

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscription } from './routes/create-transcription';
+import { generateAi } from './routes/generate-ai';
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscription)
+app.register(generateAi)
 
 app.listen({
   port: 3333
 }).then(() => {
   console.log('Http server running..');
 })
-
-// 21:46
